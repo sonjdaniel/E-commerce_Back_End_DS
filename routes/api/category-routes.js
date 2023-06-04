8e3fdd6548eba58e9bf812ee7c8abf8e6912c8e8
sonjdaniel <sonjdaniel@gmail.com>
2023-06-04 18:37:00 -0500

    Added put for catergory

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,8 +43,22 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!updateCategory) {
+      res.status(404).json({ message: "No category found with that ID" });
+      return;
+    }
+    res.status(200).json("Category name has been updated");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
